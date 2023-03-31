d7452a39c348e18f554021c1cbdaa3c3ce630a47
VetN <122635397+VetN@users.noreply.github.com>
2023-03-31 10:10:07 +0300

    структуры данных связанный список

---
+++ 9,6.3.py
@@ -0,0 +1,99 @@
+class Node:
+    '''создает элемент ы списка'''
+    def __init__(self, value = None, next_ = None):
+        self.value = value  # значение
+        self. next = next_  # ссылка на след элемент
+
+    def __str__(self):
+        return "Node value = " + str(self.value)
+
+
+class LinkedList:
+    '''реализует список'''
+    # создаем пустой список
+    def __init__(self):
+        self.first = None
+        self.last = None
+    # очищаем список
+    def clear(self):
+        self.__init__()
+    # функция печати
+    def __str__(self):
+        R = " "
+        pointer = self.first  # берем первый указатель
+        while pointer is not None:
+            R += str(pointer.value)  # добавляем значение в строку
+            pointer = pointer. next  # идем дальше по указателю
+            if pointer is not None:
+                R += " "  # если указ сущ добавл пробел
+        return R
+
+    # добавляем новы элемент в начало списка (в левую часть)
+    def pushleft(self, value):
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value, self.first)
+            self.first = new_node
+
+    # добавляем элемент в конец списка (в правую часть)
+    def pushright(self, value):
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value)
+            self.last.next = new_node
+            self.last = new_node
+
+    # удаление элемента
+    # удаление из начала путем изменения указателя на первый элемент
+    # удаление с конца нескольк способов
+    # создание двусвязного списка или
+    # модификация односвязного, сохраняя дополнительно указатель на предпоследний элемент
+    def popleft(self):
+        if self.first is None:
+            return None
+        elif self.first == self.last:
+            node = self.first  # если есть только один элемент, то сохраняем его
+            self.__init__()  # очищаем
+            return node  # возращаем сохраненный элемент
+        else:
+            node = self.first  # сохраняем первый элемент
+            self.first = self.first.next # меняем указатель на первый элемент
+            return  node # возращаем сохраненный
+
+    def popright(self):
+        if self.first is None:
+            return None
+        elif self.first == self.last:
+            node = self.first
+            self.__init__()
+            return node
+        else:
+            node = self.last  # сохраняем последний
+            pointer = self.first  # создаем указатель
+            while pointer.next is not node:  # пока не найдем последний указатель
+                pointer = pointer.next
+            pointer.next = None  # обнуляем указатели
+            self.last = pointer  #чтобы предпоследний стал последним
+            return node  #возвращаем сохраненный
+
+LL = LinkedList()
+
+LL.pushright(1)
+#print(LL)
+LL.pushleft(2)
+print(LL)
+LL.pushright(3)
+print(LL)
+LL.popright()
+print(LL)
+LL.pushleft(4)
+print(LL)
+LL.pushright(5)
+print(LL)
+LL.popleft()
+
+print(LL)
