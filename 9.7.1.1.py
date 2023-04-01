16a3ab561de48b67ce6b9f36faeab96292e66f24
VetN <122635397+VetN@users.noreply.github.com>
2023-04-01 09:52:27 +0300

    двоичный поиск

---
+++ 9.7.1.1.py
@@ -0,0 +1,21 @@
+# двоичный поиск
+def binary_search(array, element, left, right):
+    # если левая граница выше правой, значит эл-т отсут
+    if left > right:
+        return False
+
+    middle = (right + left) // 2  # находим середину
+    # если эл-т в середине, возвр этот индекс
+    if array[middle] == element:
+        return middle
+    # если эл-т меньше эл-та в середине,
+    # рекурсивно ищем в левой половине
+    elif element < array[middle]:
+        return binary_search(array, element, left, middle-1)
+    else:
+        return binary_search(array, element, middle+1, right)
+
+element = int(input())
+array = [i for i in range(1, 100)]
+
+print(binary_search(array, element, 0,99))
